056fafcf4586ee06827d98d53b05205ab21a93e2
jayantsingh-22 <jayantsingh640@gmail.com>
2023-06-01 23:55:12 +0530

    majority element

---
+++ arrays/majorityElement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int majority=nums[0];
+        int count=1;
+
+        for(int i=1;i<n;i++) {
+            if(nums[i] == majority) {
+                count+=1;
+            }
+            else {
+                count-=1;
+                if(count==0) {
+                    majority=nums[i];
+                    count=1;
+                }
+            }
+        }
+        return majority;
+    }
+};
